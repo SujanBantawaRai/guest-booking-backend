f5f5c2094013a5eced55762604ed31c3e01a9c3c
SujanBantawaRai <sujanr267@gmail.com>
2026-04-23 16:36:38 +0545

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5175', 'https://guest-booking.vercel.app' ],
+  origin: ['http://localhost:5173', 'http://localhost:5175', 'https://snehguesthouse.com.np' ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true
 }));
